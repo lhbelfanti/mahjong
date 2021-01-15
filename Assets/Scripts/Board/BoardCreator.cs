99412fb7c7869b43aaeec59c20d531bf726acb4e
Lucas <lhbelfanti@gmail.com>
2021-01-15 10:23:38 -0300

    Added floors logic.

--- Assets/Scripts/Board/BoardCreator.cs
+++ Assets/Scripts/Board/BoardCreator.cs
@@ -7,7 +7,8 @@ public class BoardCreator : MonoBehaviour
 {
 	[SerializeField] private Tile _tileGameObject;
 	[SerializeField] private Transform _boardGameObject;
-	private List<List<Tile>> _boardTiles = new List<List<Tile>>();
+	private Tile[,,] _boardTiles;
+	private GameObject[] _floors;
 
 	public void CreateBoard(int levelId)
 	{
@@ -15,27 +16,35 @@ public class BoardCreator : MonoBehaviour
 		LevelData levelData = JsonUtility.FromJson<LevelData>(levelJson.text);
 		List<LevelInfo> levelInfo = levelData.data;
 
-		List<Sprite> images = GetImages(levelInfo);
+		GetBoardSpecs(levelInfo, out int tilesCount, out Vector3 boardSize);
+		List<Sprite> images = GetImages(tilesCount);
+
+		_boardTiles = new Tile[(int) boardSize.x, (int) boardSize.y, (int) boardSize.z];
+		_floors = new GameObject[(int) boardSize.z];
+
 
 		for (int i = 0; i < levelInfo.Count; i++)
 		{
+			int f = levelInfo[i].floor;
 			List<LevelTiles> levelTiles = levelInfo[i].rows;
 			for (int j = 0; j < levelTiles.Count; j++)
 			{
-				_boardTiles.Add(new List<Tile>());
 				List<int> tiles = levelTiles[j].tiles;
 				for (int k = 0; k < tiles.Count; k++)
 				{
-					Tile tile = CreateTile(k, j, levelTiles, tiles, images);
-					_boardTiles[j].Add(tile);
+					Tile tile = CreateTile(new Vector3(k, j, f), tiles, images);
+					_boardTiles[i, j, f] = tile;
 				}
 			}
 		}
 	}
 
-	private int CountTiles(List<LevelInfo> levelInfo)
+	private void GetBoardSpecs(List<LevelInfo> levelInfo, out int tilesCount, out Vector3 boardSize)
 	{
-		int count = 0;
+		int tc = 0;
+		int rows = 0;
+		int cols = 0;
+		int floors = 0;
 		for (int i = 0; i < levelInfo.Count; i++)
 		{
 			List<LevelTiles> levelTiles = levelInfo[i].rows;
@@ -44,20 +53,29 @@ public class BoardCreator : MonoBehaviour
 				List<int> tiles = levelTiles[j].tiles;
 				for (int k = 0; k < tiles.Count; k++)
 				{
-					if (tiles[k] == 1)
-						count++;
+					if (tiles[k] != 0)
+						tc++;
 				}
+
+				if (rows < tiles.Count)
+					rows = tiles.Count;
+
 			}
+
+			if (cols < levelTiles.Count)
+				cols = levelTiles.Count;
+
+			floors++;
 		}
 
-		return count;
+		tilesCount = tc;
+		boardSize = new Vector3(rows, cols, floors);
 	}
 
-	private List<Sprite> GetImages(List<LevelInfo> levelInfo)
+	private List<Sprite> GetImages(int tilesCount)
 	{
 		Sprite[] images = Resources.LoadAll<Sprite>("Images/Tiles/");
 		List<Sprite> sprites = images.ToList();
-		int tilesCount = CountTiles(levelInfo);
 		int pairs = tilesCount / 2;
 
 		List<Sprite> totalTiles = new List<Sprite>();
@@ -74,18 +92,29 @@ public class BoardCreator : MonoBehaviour
 		return totalTiles;
 	}
 
-	private Tile CreateTile(int x, int y, List<LevelTiles> levelTiles, List<int> tiles, List<Sprite> images)
+	private Tile CreateTile(Vector3 index, List<int> tiles, List<Sprite> images)
 	{
+		int x = (int) index.x;
+		int y = (int) index.y;
+		int z = (int) index.z;
+
 		int state = tiles[x];
 		Rect tileRect = _tileGameObject.GetComponent<RectTransform>().rect;
 		Vector3 boardPos = _boardGameObject.position;
-		float xPos = boardPos.x + tileRect.width * x;
+		float xPos = boardPos.x + tileRect.width * x - z;
 		float yPos = boardPos.y - tileRect.height * y;
-		Tile tile = Instantiate(_tileGameObject, new Vector3(xPos, yPos, 0), Quaternion.identity);
+
+		Tile tile = Instantiate(_tileGameObject, new Vector3(xPos, yPos, -z), Quaternion.identity);
 		Color c = tile.Unselected;
 		tile.SpriteRenderer.color = new Color(c.r, c.g, c.b, 1);
-		tile.transform.SetParent(_boardGameObject);
+		if (!_floors[z])
+		{
+			_floors[z] = new GameObject("Floor " + z.ToString());
+			_floors[z].transform.SetParent(_boardGameObject);
+		}
+		tile.transform.SetParent(_floors[z].transform);
 		tile.transform.name = $"{x}x{y}";
+
 		if (state != 0)
 		{
 			tile.SpriteRenderer.sprite = images[0];
