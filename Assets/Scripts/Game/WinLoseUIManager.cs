bec44558534b2159733ce92609f36476a5743a8d
Lucas <lhbelfanti@gmail.com>
2021-01-17 23:57:34 -0300

    Hooked UI with win and lose scripts.

---
+++ Assets/Scripts/Game/WinLoseUIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+	[SerializeField] private SceneFader _sceneFader;
+
+	public void Retry()
+	{
+		gameObject.SetActive(false);
+		_sceneFader.FadeTo("Game");
+	}
+
+	public void Menu()
+	{
+		gameObject.SetActive(false);
+		_sceneFader.FadeTo("Main");
+	}
+}
