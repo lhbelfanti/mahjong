82ce27be625890f71178d8d045aef091cd466430
Lucas <lhbelfanti@gmail.com>
2021-02-01 23:45:17 -0300

    Editor. Added snap to grid behaviour for all the different tile types.

--- Assets/Scripts/LevelEditor/Snap.cs
+++ Assets/Scripts/LevelEditor/Snap.cs
@@ -14,43 +14,102 @@ namespace LevelEditor
 		private bool _boundsSet;
 
 		private int _smoothCounter;
-		private const int Smooth = 10;
+		private const int Smooth = 15;
 
 		private void OnDrawGizmos()
 		{
-			if (!Application.isPlaying && transform.hasChanged && _boundsSet && _smoothCounter == Smooth)
+			if (_boundsSet && _smoothCounter == Smooth)
 			{
 				_smoothCounter = 0;
-				switch (_tileState)
-				{
-					case TileCreator.TileStates.Single:
-						SnapToGridSingle();
-						break;
-					case TileCreator.TileStates.DoubleH:
-						SnapToGridDoubleH();
-						break;
-					case TileCreator.TileStates.DoubleV:
-						SnapToGridDoubleV();
-						break;
-				}
+				transform.position = RoundToNearestCell(transform.position);
 			}
 
 			_smoothCounter++;
 		}
 
-		private void SnapToGridSingle()
+		private void SetBounds()
 		{
-			transform.position = RoundToNearestCell(transform.position);
+			Rect rect = gridCell.rect;
+			float xOffset = 0f;
+			float yOffset = 0f;
+
+			_widthBound = new Vector2[(int) _boardSize.x];
+			_heightBound = new Vector2[(int) _boardSize.y];
+
+			switch (_tileState)
+			{
+				case TileCreator.TileStates.DoubleH:
+					_widthBound = new Vector2[(int) _boardSize.x - 1];
+					xOffset = rect.width / 2;
+					break;
+				case TileCreator.TileStates.DoubleV:
+					_heightBound = new Vector2[(int) _boardSize.y - 1];
+					yOffset = rect.height / 2;
+					break;
+			}
+
+			for (int i = 0; i < _widthBound.Length; i++)
+			{
+				float leftBound = i == 0 ? rect.width * i + xOffset: _widthBound[i - 1].y;
+				float rightBound = leftBound + (i != _widthBound.Length - 1 ? rect.width + GridEditor.Gap : rect.width);
+				_widthBound[i] = new Vector2(leftBound, rightBound);
+			}
+
+			for (int i = 0; i < _heightBound.Length; i++)
+			{
+				float topBound = i == 0 ? -(rect.height * i) - yOffset : _heightBound[i - 1].y;
+				float bottomBound = topBound + (i != _heightBound.Length - 1 ? -(rect.height + GridEditor.Gap) : -rect.height);
+				_heightBound[i] = new Vector2(topBound, bottomBound);
+			}
+
+			_boundsSet = true;
 		}
 
-		private void SnapToGridDoubleH()
+		private Vector3 RoundToNearestCell(Vector3 pos)
 		{
-			// TODO
+			float xPos = RoundX(pos.x);
+			float yPos = RoundY(pos.y);
+			return new Vector3(xPos, yPos, pos.z);
 		}
 
-		private void SnapToGridDoubleV()
+		private float RoundX(float x)
 		{
-			// TODO
+			float xPos = x;
+			if (xPos < _widthBound[0].x)
+				xPos = _widthBound[0].x;
+			else if (xPos > _widthBound[_widthBound.Length - 1].x)
+				xPos = _widthBound[_widthBound.Length - 1].x;
+
+			if (xPos == x)
+			{
+				for (int i = 0; i < _widthBound.Length; i++)
+				{
+					Vector2 value = _widthBound[i];
+					if (xPos > value.x && xPos < value.y)
+						xPos = value.x;
+				}
+			}
+			return xPos;
+		}
+
+		private float RoundY(float y)
+		{
+			float yPos = y;
+			if (yPos > _heightBound[0].x)
+				yPos = _heightBound[0].x;
+			else if (yPos < _heightBound[_heightBound.Length - 1].x)
+				yPos = _heightBound[_heightBound.Length - 1].x;
+
+			if (yPos == y)
+			{
+				for (int i = 0; i < _heightBound.Length; i++)
+				{
+					Vector2 value = _heightBound[i];
+					if (yPos < value.x && yPos > value.y)
+						yPos = value.x;
+				}
+			}
+			return yPos;
 		}
 
 		public TileCreator.TileStates TileState
@@ -62,66 +121,6 @@ namespace LevelEditor
 		{
 			_boardSize = value;
 			SetBounds();
-		}
-
-		private void SetBounds()
-		{
-			Rect rect = gridCell.rect;
-			_widthBound = new Vector2[(int) _boardSize.x];
-			_heightBound = new Vector2[(int) _boardSize.y];
-
-			for (int i = 0; i < _widthBound.Length; i++)
-			{
-				float xValue = i == 0 ? rect.width * i : _widthBound[i - 1].y;
-				float yValue = xValue + (i != _widthBound.Length - 1 ? rect.width + GridEditor.Gap : rect.width);
-				_widthBound[i] = new Vector2(xValue, yValue);
-			}
-
-			for (int i = 0; i < _heightBound.Length; i++)
-			{
-				float xValue = i == 0 ? -(rect.height * i) : _heightBound[i - 1].y;
-				float yValue = xValue + (i != _heightBound.Length - 1 ? -(rect.height + GridEditor.Gap) : -rect.height);
-				_heightBound[i] = new Vector2(xValue, yValue);
-			}
-
-			_boundsSet = true;
-		}
-
-		private Vector3 RoundToNearestCell(Vector3 pos)
-		{
-			float xPos = pos.x;
-			if (xPos < _widthBound[0].x)
-				xPos = _widthBound[0].x;
-			else if (xPos > _widthBound[_widthBound.Length - 1].x)
-				xPos = _widthBound[_widthBound.Length - 1].x;
-
-			if (xPos == pos.x)
-			{
-				for (int i = 0; i < _widthBound.Length; i++)
-				{
-					Vector2 value = _widthBound[i];
-					if (xPos > value.x && xPos < value.y)
-						xPos = value.x;
-				}
-			}
-
-			float yPos = pos.y;
-			if (yPos > _heightBound[0].x)
-				yPos = _heightBound[0].x;
-			else if (yPos < _heightBound[_heightBound.Length - 1].x)
-				yPos = _heightBound[_heightBound.Length - 1].x;
-
-			if (yPos == pos.y)
-			{
-				for (int i = 0; i < _heightBound.Length; i++)
-				{
-					Vector2 value = _heightBound[i];
-					if (yPos < value.x && yPos > value.y)
-						yPos = value.x;
-				}
-			}
-
-			return new Vector3(xPos, yPos, pos.z);
 		}
 	}
 }
